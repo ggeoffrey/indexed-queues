84ae87ee7e065860728c370a413e121d2949e7ad
Geoffrey Gaillard <geoffrey.gaillard@brainsfeed.com>
2020-01-26 17:22:52 +0100

    Add deps

--- project.clj
+++ project.clj
@@ -3,5 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[chazel "0.1.18"]
+                 [manifold "0.1.8"]
+                 [org.clojure/clojure "1.10.0"]
+                 [org.clojure/core.cache "0.8.2"]]
   :repl-options {:init-ns indexed-queue.core})
